37af09e9839d2829047b7a30527ac37bb8e7f28e
bimashazaman <bimashazaman1234.com>
2023-07-31 03:10:54 +0600

    useUser

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Figtree } from 'next/font/google'
 import Sidebar from '@/components/partials/Sidebar'
+import SupabaseProvider from '@/providers/SuperbaseProvider'
 
 const font = Figtree({ subsets: ['latin'] })
 
@@ -19,11 +20,13 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={font.className}>
-        <Sidebar>
-          <main className='flex flex-col min-h-screen'>
-            <div className='flex-grow'>{children}</div>
-          </main>
-        </Sidebar>
+        <SupabaseProvider>
+          <Sidebar>
+            <main className='flex flex-col min-h-screen'>
+              <div className='flex-grow'>{children}</div>
+            </main>
+          </Sidebar>
+        </SupabaseProvider>
       </body>
     </html>
   )
